c659912dd20322863217cd65bb069e14e28117fd
robertoyoc <a01172599@itesm.mx>
2017-04-19 02:11:32 -0500

    interface colors changed

---
+++ app/src/main/java/talentics/com/mx/zhake/layout_request.java
@@ -0,0 +1,13 @@
+package talentics.com.mx.zhake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class layout_request extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_request);
+    }
+}
